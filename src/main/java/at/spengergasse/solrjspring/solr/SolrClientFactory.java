0ab0c5dcbd6be00bcfd8e925b8c6748f9edfec17
Maximilian Kraft <kraft@spengergasse.at>
2024-05-02 10:30:58 +0200

    using Environment instead of Properties

--- src/main/java/at/spengergasse/solrjspring/solr/SolrClientFactory.java
+++ src/main/java/at/spengergasse/solrjspring/solr/SolrClientFactory.java
@@ -1,32 +1,32 @@
 package at.spengergasse.solrjspring.solr;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.impl.XMLResponseParser;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
 @Component
 @Log
+@AllArgsConstructor
 public class SolrClientFactory {
+
+    Environment environment;
 
     @Bean
     public HttpSolrClient solrBean() throws IOException {
         Properties prop = new Properties();
 
-        InputStream inputStream = getClass().getResourceAsStream("/application.properties");
-        prop.load(inputStream);
-
-        return SolrClientFactory.connect(prop);
+        return SolrClientFactory.connect(environment);
     }
 
-    public static HttpSolrClient connect(Properties props){
-
-        String urlString = "http://"+props.getProperty("cli.solrhostname")+":"+props.getProperty("cli.solrhostport")+"/solr/"+props.getProperty("cli.solrcollection");
+    public static HttpSolrClient connect(Environment environment){
+        String urlString = "http://"+environment.getProperty("cli.solrhostname")+":"+environment.getProperty("cli.solrhostport")+"/solr/"+environment.getProperty("cli.solrcollection");
 
         log.info("Url string to connect to");
         log.info(urlString);
